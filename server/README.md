e8126818d4d26e5cdcfd35fc5fc8e7ecd35ae381
Tigran Muradyan <tigran.muradyan140201@gmail.com>
2023-12-02 17:41:35 -0800

    Server configurations added

---
+++ server/README.md
@@ -0,0 +1,11 @@
+# TextAI API - Words into Worlds 💬 🌎
+
+> "The eyes chico they never lie..." - Tony Montana
+
+## Overview
+
+TextAI is a platform that generates images from text input using cutting-edge AI technology. Users can input any text, and the platform will generate visually compelling images based on the text content.
+
+## Getting Started
+
+1. Clone `.env.example` to `.env` and fill in the required environment variables.
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello Node!");
+import bodyParser from "body-parser";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import express from "express";
+import http from "http";
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+const port = process.env.PORT || 8080;
+
+server.listen(port, () => {
+  console.log(`Server running on localhost:${port}`);
+});
